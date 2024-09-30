793ba28397b80408385ecfd3e71c955a4da63bbe
Rafael Sabatini <bonieliasabatini@gmail.com>
2024-09-30 09:00:18 -0300

    Arquivos API

---
+++ src/main/java/com/control/cash/CashApplication.java
@@ -0,0 +1,13 @@
+package com.control.cash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/control/cash/CashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control.cash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
