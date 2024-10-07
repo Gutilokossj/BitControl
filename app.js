036272e910e8aa7630518a60c19db466a316ef9f
Gustavo Henrique Braga Fernandes <gutilokossj12@gmail.com>
2024-10-07 17:12:58 -0300

    Primeiro commit, subir para testes QA

---
+++ app.js
@@ -0,0 +1,15 @@
+var btnSignin = document.querySelector("#login");
+var btnSignup = document.querySelector("#Cadastrar");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
+
+
