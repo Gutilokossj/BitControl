a1d3191d91afa2cc03d7b2fdda5776f692b881b5
Rafael Sabatini <bonieliasabatini@gmail.com>
2024-10-10 16:38:51 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
-var btnSignin = document.querySelector("#login");
-var btnSignup = document.querySelector("#Cadastrar");
+var loginBtn = document.getElementById("loginBtn");
+var saveBtn = document.getElementById("saveBtn");
+var cadastrarBtn = document.getElementById("cadastrarBtn")
 
 var body = document.querySelector("body");
 
+const nomeCompleto = document.getElementById("nomeCompleto").value;
+const email = document.getElementById("email").value;
+const senha = document.getElementById("senha").value;
+const telefone = document.getElementById("telefone").value;
 
-btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
-});
+const apiUrl = "http://localhost:8080/login";
 
-btnSignup.addEventListener("click", function () {
-    body.className = "sign-up-js";
+var jsonData = `{"nomeCompleto": "${nomeCompleto}", "email":"${email}", "senha":"${senha}", "telefone":"${telefone}"}`;
+var obj = JSON.parse(jsonData);
+
+loginBtn.addEventListener('click', () =>{
+    body.className = "sign-in-js";
 })
 
+cadastrarBtn.addEventListener('click', () =>{
+    body.className = "sign-up-js";
+})
+//Método POST, Enviar JSON para API
+saveBtn.addEventListener("click", function (event) {
+    event.preventDefault();
 
+    fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(obj),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log("User successfully registered!");
+        } else {
+            console.error("Error: " + response.status);
+        }
+    })
+    .catch(error => console.error("Fetch error:", error));
+});
